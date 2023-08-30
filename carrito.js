45ac9a98319efc39148a2f70c37bf16a595a6392
Jesus <jesusvazquez2477@gmail.com>
2023-08-30 14:45:00 +0200

    Implemento funciones para pintar de color verde o rojo cuando se selecciona o elimina algun plato, implemento funcion para actualizar el modal una vez realizado el pedido, implemento funcion para mostrar mensaje de exito

--- carrito.js
+++ carrito.js
@@ -56,9 +56,9 @@ function mostrarCarrito() {
         <span class="dish-name">${item.nombre}</span>
         <span class="dish-precio">${item.precio} €</span>
         <span class="dish-cantidad">${item.cantidad}</span>
-        <button class="btn btn-sm btn-danger btn-quitar" data-nombre="${item.nombre}">
-          <i class="fas fa-minus"></i>
-        </button>
+        <img class="eliminarBoton btn-icon btn-quitar" src="../img/eliminar.png" alt="Remove" data-nombre="${item.nombre}">
+        <i class="fas fa-minus"></i>
+        </img>
       `;
     carritoContainer.appendChild(platoItem);
   });
@@ -78,6 +78,11 @@ imagenesAgregar.forEach((imagen) => {
     const nombre = imagen.dataset.nombre;
     const precio = parseFloat(imagen.dataset.precio);
     agregarAlCarrito(nombre, precio);
+    // Agregar la clase "selected" para activar la animación
+    imagen.classList.add("flash-green");
+    setTimeout(() => {
+      imagen.classList.remove("flash-green");
+    }, 500);
   });
 });
 // Agregar eventos de clic a las imágenes "Quitar del carrito"
@@ -85,7 +90,14 @@ const imagenesQuitar = document.querySelectorAll(".btn-icon.btn-quitar");
 imagenesQuitar.forEach((imagen) => {
   imagen.addEventListener("click", () => {
     const nombre = imagen.dataset.nombre;
-    quitarDelCarrito(nombre);
+    const platoExistente = carrito.find((item) => item.nombre === nombre);
+    if (platoExistente && platoExistente.cantidad > 0) {
+      quitarDelCarrito(nombre);
+      imagen.classList.add("flash-red");
+      setTimeout(() => {
+        imagen.classList.remove("flash-red");
+      }, 500);
+    }
   });
 });
 // Función para calcular y mostrar el total de los platos en el carrito
@@ -95,7 +107,48 @@ function mostrarTotal() {
     (total, item) => total + item.precio * item.cantidad,
     0
   );
-  totalCarritoModal.textContent = `Total: ${total.toFixed(2)} €`;
+  totalCarritoModal.textContent = `Total a pagar: ${total.toFixed(2)} €`;
 }
-// Llamada a la función inicial para mostrar el carrito vacío
+const realizarPedidoButton = document.getElementById("realizar-pedido");
+const successMessage = document.getElementById("success-message");
+const exitoMessage = document.getElementById("exito-message");
+// Función para mostrar el mensaje de éxito y luego reiniciar el carrito y el mensaje
+function mostrarMensajeExito() {
+  exitoMessage.style.display = "block";
+  setTimeout(() => {
+    exitoMessage.style.display = "none";
+    carrito.length = 0;
+    mostrarCarrito();
+  }, 2000);
+}
+// Función para actualizar el contenido del modal
+function actualizarModal() {
+  if (carrito.length > 0) {
+    successMessage.style.display = "block";
+    exitoMessage.style.display = "none";
+  } else {
+    successMessage.style.display = "none";
+    exitoMessage.style.display = "none";
+    realizarPedidoButton.style.display = "block";
+  }
+}
+realizarPedidoButton.addEventListener("click", () => {
+  // Verificar si hay elementos en el carrito
+  if (carrito.length > 0) {
+    // Mostrar mensaje de éxito y ocultar botón
+    mostrarMensajeExito(); // Mostrar el mensaje de éxito
+    successMessage.style.display = "none";
+    realizarPedidoButton.style.display = "none";
+    // Limpiar el carrito y actualizar la interfaz
+    carrito.length = 0;
+    mostrarCarrito();
+    mostrarTotal();
+    closeModal();
+  } else {
+    // Si no hay elementos en el carrito, muestra un mensaje de advertencia
+    alert("El carrito está vacío. Agrega platos antes de realizar el pedido.");
+  }
+  actualizarModal();
+});
+
 mostrarCarrito();
