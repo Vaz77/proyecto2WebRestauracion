3380da82e2e695191bea401875acb5f921f86d9a
Jesus <jesusvazquez2477@gmail.com>
2023-09-05 21:20:34 +0200

    Implemento funcion para mostrar el boton despues de los textos de inicio y actualizo estilos del boton

--- textoInicio.js
+++ textoInicio.js
@@ -1,5 +1,5 @@
 const textoArray = [
-  "Sala para cocteles ambientada",
+  "Sala de cocteles ambientada",
   "Prueba nuestra experiencia Street Food",
   "Reparto a domicilio!",
 ];
@@ -19,7 +19,35 @@ function detenerParpadeoDespuesDe15Segundos() {
   iniciarParpadeo();
   setTimeout(function () {
     clearInterval(parpadeoInterval);
+    mostrarBotonCarta();
   }, 15000);
 }
 
+function mostrarBotonCarta() {
+  const botonCarta = document.getElementById("botonCarta");
+  botonCarta.style.display = "block";
+}
+
 detenerParpadeoDespuesDe15Segundos();
+
+function fadeIn(element) {
+  let opacity = 0;
+  element.style.display = "block";
+  const intervalId = setInterval(function () {
+    if (opacity >= 1) {
+      clearInterval(intervalId);
+    } else {
+      opacity += 0.02;
+      element.style.opacity = opacity;
+    }
+  }, 20);
+}
+
+const botonCarta = document.getElementById("botonCarta");
+botonCarta.addEventListener("click", () => {
+  botonCarta.classList.toggle("neon-effect");
+});
+
+setTimeout(function () {
+  fadeIn(botonCarta);
+}, 14000);
